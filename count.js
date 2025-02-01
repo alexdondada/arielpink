6cff86935107a23396adbb60254898774682350c
alexdondada <arider187@gmail.com>
2025-02-01 18:25:27 +0200

    Update count.js

--- count.js
+++ count.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Set the date we're counting down to
-  var countDownDate = new Date("Jan 31, 2025 15:37:25").getTime();
+  var countDownDate = new Date("Feb 28, 2025 15:37:25").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function() {
